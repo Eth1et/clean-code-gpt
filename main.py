91a910a33530f2e22efbcf60fa00d142f0f47a80
Eth1et <bodimartin22@gmail.com>
2023-09-27 21:44:18 +0200

    test commit

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import openai
+import json
+
+def get_response_content(response):
+    return response['choices'][0]['message']['content']
+
+CONFIG = json.load(open("config.json"))
+
+MODEL = CONFIG['model']
+openai.api_key = CONFIG['api_key']
+openai.organization = CONFIG['organization']
+
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who is considered to be the best cs2 player so far?"}
+    ]
+)
+
+print(get_response_content(response))
