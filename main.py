5095daa6af6f674d44c0cc9bc8700fd14b50d6e9
Eth1et <bodimartin22@gmail.com>
2023-09-30 17:13:06 +0200

    first somewhat working version

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import openai
 import json
 import argparse
 from pathlib import Path
+from shutil import copy2
 import os
 
 
 class CleanedCode:
+    
         original = ""
         clean = ""
         
@@ -15,6 +17,7 @@ class CleanedCode:
 
 
 class CleanedFile:  
+    
     fragments = []
     filename = ""
     dirname = ""
@@ -30,18 +33,27 @@ def get_response_content(response):
 
 
 def apply_config():
-    CONFIG = json.load(open("config.json"))
+    
+    with open("config.json") as cfg:
+        CONFIG = json.load(cfg)
+        openai.api_key = CONFIG['api_key']
+        openai.organization = CONFIG['organization']
+        return CONFIG['model']
 
-    openai.api_key = CONFIG['api_key']
-    openai.organization = CONFIG['organization']
-    return CONFIG['model']
+
+def get_extensions():
+    
+    with open('language_extensions.json', 'r') as extensions:
+        return json.load(extensions)['extensions']
 
 
 def parse_args():
+    
     parser = argparse.ArgumentParser()
     parser.version = '0.1'
 
     parser.add_argument('-e','--encoding', choices=('ascii', 'utf-7', 'utf-8', 'utf-16', 'utf-32'), default='utf-8')
+    parser.add_argument('-cd','--copy_data', action='store_true', default=False)
     
     input_group = parser.add_mutually_exclusive_group(required=True)
     input_group.add_argument('-f', '--file', action='store')
@@ -54,33 +66,59 @@ def parse_args():
     return vars(parser.parse_args())
 
 
+def clean_file(file_path, input_path, encoding):
+    
+    with file_path.open('r', encoding=encoding) as file:
+        code = ''.join(file.readline())
+        
+        ## TODO: handle too large input
+        
+        ## TODO: handle API exception
+        
+        ## TODO: call API
+        
+        cleanedFile = CleanedFile(
+            [CleanedCode(code, "problem solved kekw")], 
+            file_path.name, 
+            file_path.relative_to(input_path).parent)
+        
+        return cleanedFile
+
+
 def main():
+    
     MODEL = apply_config()
     ARGS = parse_args()
-    
+    LANGUAGE_EXTENSIONS = get_extensions()
+
     INPUT_PATH = Path(ARGS['file']).parent  if ARGS['file'] != None  else Path(ARGS['dir']) 
     INPUT_PATH = INPUT_PATH.absolute()
-    OUTPUT_PATH = ARGS['out']  if ARGS['out'] != None  else (INPUT_PATH / 'clean_code')
+    OUTPUT_PATH = Path(ARGS['out'])  if ARGS['out'] != None  else (INPUT_PATH / 'clean_code')
     
     cleanedFiles = []
+    copiedFiles = []
     
-    file = open(ARGS['file'], "r", encoding=ARGS['encoding'])
-    code = "".join(file.readlines())
+    # Cleaning code files, marking others for copy
+    if ARGS['file'] == None:
+        for entry in Path(INPUT_PATH).glob('**/*'):
+            
+            if entry.is_file() and entry.suffix.lower() in LANGUAGE_EXTENSIONS:
+                cleanedFiles.append(clean_file(entry, INPUT_PATH, ARGS['encoding']))
+            
+            elif entry.is_file() and ARGS['copy_data']:
+                copiedFiles.append(entry)
+                
+    else:
+        cleanedFiles.append(clean_file(Path(INPUT_PATH) / Path(ARGS['file']).name, INPUT_PATH, ARGS['encoding']))
     
-    # TODO: handling long input
-    
-    # TODO: handle API call fail
-    ## if error occours -> ERROR message
-    
-    # TODO: API call
-    
-    cleanedFiles.append(CleanedFile(
-        [CleanedCode(code, "problem solved xd")], 
-        Path(ARGS['file']).name,
-        Path(ARGS['file']).parent.absolute().relative_to(INPUT_PATH)))
-    
-    # Creating outut directory
-    Path(OUTPUT_PATH).mkdir()
+    # Handling output directory
+    if Path(OUTPUT_PATH).exists():
+        
+        if any(Path(OUTPUT_PATH).iterdir()):
+            print(f"[ERROR] The output directory is not empty! ( {OUTPUT_PATH} )")
+            
+    else:
+        Path(OUTPUT_PATH).mkdir(parents=True)
     
     # Writing output into files
     for cleanedFile in cleanedFiles:
@@ -89,13 +127,23 @@ def main():
         for fragment in cleanedFile.fragments:
             clean_code = clean_code + fragment.clean
         
-        (Path(OUTPUT_PATH) / cleanedFile.dirname / cleanedFile.filename).write_text(clean_code, encoding=ARGS['encoding'])
-    
+        dir = Path(OUTPUT_PATH) / cleanedFile.dirname
+        if not dir.exists():
+            dir.mkdir(parents=True)
+        
+        (dir / cleanedFile.filename).write_text(clean_code, encoding=ARGS['encoding'])
+        
+    # Copying marked files
+    if ARGS['copy_data']:
+        for copiedFile in copiedFiles:
+            copy2(copiedFile, OUTPUT_PATH)
+        
 
 if __name__ == "__main__":
+    
     try:
         main()
     except Exception as e:
         print(e)
 
-    input()
+    input('\n\nPress Enter to exit!')
