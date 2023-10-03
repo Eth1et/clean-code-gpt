e2d88b2c4ff4aa90403b115ff08b14a9b61f6dff
Ethiet@Laptop <bodimartin22@gmail.com>
2023-10-03 16:58:01 +0200

    major changes
    +console only output
    +token count
    ~moved dir empty check to start
    ~cleaned the code

--- main.py
+++ main.py
@@ -3,56 +3,55 @@ import json
 import argparse
 from pathlib import Path
 from shutil import copy2
+import tiktoken
+
+
+MODEL = ""
+INSTRUCTION = ""
+ARGS = {}
+LANGUAGE_EXTENSIONS = []
+ENCODER = None
+INPUT_PATH = ""
+OUTPUT_PATH = ""
 
 
 class CleanedCode:
-    
-        original = ""
-        clean = ""
-        
-        def __init__(self, original, clean):
-            self.original = original
-            self.clean = clean
+    def __init__(self, original, clean):
+        self.original = original
+        self.clean = clean
 
 
 class CleanedFile:  
-    
-    fragments = []
-    filename = ""
-    dirname = ""
-    
     def __init__(self, fragments, filename, dirname) -> None:
         self.fragments = fragments
         self.filename = filename
         self.dirname = dirname
     
 
-def get_response_content(response):
-    return response['choices'][0]['message']['content']
-
-
 def apply_config():
+    global MODEL, INSTRUCTION
     
     with open("config.json") as cfg:
         CONFIG = json.load(cfg)
         openai.api_key = CONFIG['api_key']
         openai.organization = CONFIG['organization']
-        return CONFIG['model'], CONFIG['instruction']
+        MODEL, INSTRUCTION = CONFIG['model'], CONFIG['instruction']
 
 
-def get_extensions():
+def set_extensions():
+    global LANGUAGE_EXTENSIONS
     
     with open('language_extensions.json', 'r') as extensions:
-        return json.load(extensions)['extensions']
+        LANGUAGE_EXTENSIONS = json.load(extensions)['extensions']
 
 
 def parse_args():
+    global ARGS
     
     parser = argparse.ArgumentParser()
     parser.version = '0.1'
 
     parser.add_argument('-e','--encoding', choices=('ascii', 'utf-7', 'utf-8', 'utf-16', 'utf-32'), default='utf-8')
-    parser.add_argument('-p','--preserve', action='store_true', default=False)
     
     input_group = parser.add_mutually_exclusive_group(required=True)
     input_group.add_argument('-f', '--file', action='store')
@@ -62,87 +61,66 @@ def parse_args():
     output_group.add_argument('-c','--console', action='store_true')
     output_group.add_argument('-o','--out', action='store')
     
-    return vars(parser.parse_args())
-
-
-def chat_completion(code, model, instruction):
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[
-        {"role": "system", "content": instruction},
-        {"role": "user", "content": code},
-    ])
+    preserve_group = parser.add_mutually_exclusive_group()
+    preserve_group.add_argument('-p','--preserve', action='store_true', default=False)
+    preserve_group.add_argument('-c','--console', action='store_true')
     
-    print(response)
+    ARGS = vars(parser.parse_args())
+
+
+def chat_completion(messages):
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=messages
+    )
     
     return response['choices'][0]['message']['content']
 
 
-def clean_file(file_path, input_path, encoding, model, instruction):
+def clean_file(file_path, encoding):
     
     with file_path.open('r', encoding=encoding) as file:
-        code = ''.join(file.readline())
+        code = ''.join(file.readlines())
         
+        messages = [
+            {"role" : "system", "content" : INSTRUCTION},
+            {"role" : "user", "content" : code}
+        ]
+
+        num_tokens = 3
+        for message in messages:
+            num_tokens += 3
+            for key, value in message.items():
+                num_tokens += len(ENCODER.encode(value))
+                if key == "name":
+                    num_tokens += 1
+                    
+        print(num_tokens)
         ## TODO: handle too large input
         
-        clean_code = chat_completion(code, model, instruction)
+        clean_code = chat_completion(messages)
         
         cleanedFile = CleanedFile(
             [CleanedCode(code, clean_code)], 
             file_path.name, 
-            file_path.relative_to(input_path).parent)
+            file_path.relative_to(INPUT_PATH).parent)
         
         return cleanedFile
 
 
-def main():
-    
-    MODEL, INSTRUCTION = apply_config()
-    ARGS = parse_args()
-    LANGUAGE_EXTENSIONS = get_extensions()
-
-    INPUT_PATH = Path(ARGS['file']).parent  if ARGS['file'] != None  else Path(ARGS['dir']) 
-    INPUT_PATH = INPUT_PATH.absolute()
-    OUTPUT_PATH = Path(ARGS['out'])  if ARGS['out'] != None  else (INPUT_PATH.parent / 'clean_code')
-    
-    cleanedFiles = []
-    copiedFiles = []
-
-    # Cleaning code files, marking others for copy
-    for entry in Path(INPUT_PATH).glob('**/*'):
-        
+def clean_files(cleanedFiles, copiedFiles):
+    for entry in Path(INPUT_PATH).glob('**/*'):    
         if entry.is_file():
             if entry.suffix.lower() in LANGUAGE_EXTENSIONS and (ARGS['file'] == None or Path(ARGS['file']).samefile(entry)):
             
-                file = entry  if ARGS['file'] == None  else (INPUT_PATH / Path(ARGS['file']).name) 
-                cleanedFiles.append(clean_file(file, INPUT_PATH, ARGS['encoding'], MODEL, INSTRUCTION))
-            
+                file = entry  if ARGS['file'] is None  else (INPUT_PATH / Path(ARGS['file']).name) 
+                cleanedFiles.append(clean_file(file, ARGS['encoding']))
+
             elif ARGS['preserve']:
-                copiedFiles.append(entry)       
-    
-    # Handling output directory
-    if Path(OUTPUT_PATH).exists():
-        
-        if any(Path(OUTPUT_PATH).iterdir()):
-            print(f"[ERROR] The output directory is not empty! ( {OUTPUT_PATH} )")
-            
-    else:
-        Path(OUTPUT_PATH).mkdir(parents=True)
-    
-    # Writing output into files
-    for cleanedFile in cleanedFiles:
-        clean_code = ""
-        
-        for fragment in cleanedFile.fragments:
-            clean_code = clean_code + fragment.clean
-        
-        dir = Path(OUTPUT_PATH) / cleanedFile.dirname
-        if not dir.exists():
-            dir.mkdir(parents=True)
-        
-        (dir / cleanedFile.filename).write_text(clean_code, encoding=ARGS['encoding'])
-        
-    # Copying marked files
+                copiedFiles.append(entry)
+
+
+def copy_preserved(copiedFiles):
     if ARGS['preserve']:
         
         for copiedFile in copiedFiles:   
@@ -153,13 +131,57 @@ def main():
                 dir.mkdir(parents=True)
             
             copy2(copiedFile, dir / copiedFile.name)
+
+
+def write_output(cleanedFiles):
+    for cleanedFile in cleanedFiles:
+        clean_code = ""
+        
+        for fragment in cleanedFile.fragments:
+            clean_code = clean_code + fragment.clean
+        
+        if ARGS['console']:
+            path = Path(cleanedFile.dirname) / cleanedFile.filename
+            print(f"\n\n{'='*15} START OF <{path}>{'='*15}\n{clean_code}\n{'='*15} END OF <{path}>;{'='*15}\n\n")
+        else:
+            dir = Path(OUTPUT_PATH) / cleanedFile.dirname
+            if not dir.exists():
+                dir.mkdir(parents=True)
+
+            (dir / cleanedFile.filename).write_text(clean_code, encoding=ARGS['encoding'])
+
+
+def main():
+    global INPUT_PATH, OUTPUT_PATH, ENCODER
+    
+    apply_config()
+    parse_args()
+    set_extensions()
+    
+    ENCODER = tiktoken.encoding_for_model(MODEL)
+    INPUT_PATH = Path(ARGS['file']).parent  if ARGS['file'] != None  else Path(ARGS['dir']) 
+    INPUT_PATH = INPUT_PATH.absolute()
+    OUTPUT_PATH = Path(ARGS['out'])  if ARGS['out'] != None  else (INPUT_PATH.parent / 'clean_code')
+    
+    cleanedFiles = []
+    copiedFiles = []
+
+    if Path(OUTPUT_PATH).exists():    
+        if any(Path(OUTPUT_PATH).iterdir()):
+            print(f"[ERROR] The output directory is not empty! ( {OUTPUT_PATH} )")
+            return
+    else:
+        Path(OUTPUT_PATH).mkdir(parents=True)
+
+    clean_files(cleanedFiles, copiedFiles)       
+    write_output(cleanedFiles)
+    copy_preserved(copiedFiles)
         
 
 if __name__ == "__main__":
-    
     try:
         main()
     except Exception as e:
         print(e)
-
-    input('\n\nPress Enter to exit!')
+        
+    input("Press Enter to quit!")
