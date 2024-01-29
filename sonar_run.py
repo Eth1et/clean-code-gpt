64cb2a52dde7f539463d446be53ba99e208ca546
Ethiet <bodimartin22@gmail.com>
2024-01-29 15:52:37 +0100

    sonarqube done

---
+++ sonar_run.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+def main():
+    subprocess.run([
+        'docker', 'run', '-d', '--name', 'sonarqube', 
+        '-e', 'SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true', 
+        '-p', '9000:9000', 
+        'sonarqube:lts-community'
+    ])
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
